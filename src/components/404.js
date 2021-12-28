e6f4ccc4dba75344280eae9d7ed1b3f3cafffb39
Beau Hibbert <beauhibbert789@gmail.com>
2021-12-27 18:16:36 -0800

    routing in progress

---
+++ src/components/404.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is a 404 page</h2>
+      </div>
+    );
+  }
+}
